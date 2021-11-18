c911e5489c2d254cfe4c7c845b68b4afc21c9b5d
Lucas Oliveira <lucasoliveira226@gmail.com>
2021-11-18 16:32:18 -0300

    [GRUPO 24] - Requisito 1: getCategories pronto, getProducts incompleto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <h1>Tela inicial</h1>
+        {console.log(getCategories())}
+        {console.log(getProductsFromCategoryAndQuery())}
       </div>
     );
   }
